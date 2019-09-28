5bfb61e6ace5daf7850f8a6ba71e82855cf21994
MinlleSeok <32080412+MinlleSeok@users.noreply.github.com>
2019-09-28 14:08:29 +0900

    Create Code01.java

---
+++ src/lecture/recursion/Code01.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class Code01 {
+	public static void main(String[] args) {
+	    int n = 4;
+		func(n);
+}
+
+	public static void func(int k) {
+		if (k <= 0)
+			return;
+		else {
+			System.out.println("Hello...");
+			func(k-1);
+		}
+    }
+ }
